e99adcbbc57602a2f51aa9aa1d3ab1e4020d013c
Christopher Voss <voss@ins.uni-bonn.de>
2019-07-14 19:18:49 +0200

    list option working

--- util.py
+++ util.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
-import json
-import os
+import sys, json, os, subprocess, re
 
 def print_usage():
   #TODO
@@ -18,11 +16,34 @@ def print_usage():
   """
   print(usage_str)
 
+def get_factorio_version(path_to_exe):
+  """Returns the version of the factorio executable given by parameter"""
+  exe_version = str(subprocess.check_output([path_to_exe, "--version"]))
+  exe_version = re.search("Version: (\d+\.\d+\.\d+)", exe_version)
+  exe_version = exe_version.group(1)
+  return exe_version
+
 def list_servers(SETTINGS):
+  """Lists version and enabled mods of all servers found in SETTINGS.serverdir"""
+  factorio_servers = []
   server_dirs = os.listdir(SETTINGS["serverdir"])
   for server in server_dirs:
-    # list version and enabled mods
-    print(server)
+    # check if actual factorio server directory or not
+    if not os.path.isdir(f"{SETTINGS['serverdir']}/{server}"):
+      continue
+    if not os.path.isfile(f"{SETTINGS['serverdir']}/{server}/bin/x64/factorio"):
+      continue
+    factorio_servers.append(server)
+  if len(factorio_servers) == 0:
+    print(f"No factorio servers found in {SETTINGS['serverdir']}")
+    return
+
+  # now used cleaned up list
+  for server in factorio_servers:
+    exe_path = f"{SETTINGS['serverdir']}/{server}/bin/x64/factorio"
+    exe_version = get_factorio_version(exe_path)
+    print(f"{server}:")
+    print(f" Version: {exe_version}")
   return
 
 def load_settings():
